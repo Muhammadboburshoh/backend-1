1642fb96b51977b7ba7216e618f711aad071fc54
Muhammadboburshoh <muhammadbobur600@gmail.com>
2020-12-09 10:57:21 +0500

    vardan vos kechis

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,50 @@
-//GlobalScope - kodning hammaga ko'rinib turgan qismi
+/* //GlobalScope - kodning hammaga ko'rinib turgan qismi
+
+console.log("global");
+const PI = 3.14;
 
 {
+  console.log("local 1");
+  const PI = 0;
+  // const PI = 0; SentaxError
   //localScope - har bir xona localScope deyiladi
 }
 
+console.log(PI);
+
+//------------
+
+let lang = "en";
+
+{
+  console.log(lang)
+  
+  let lang = "uz"
+
+  console.log();
+}
+
+console.log(lang);
+
+var abdurahmon = "abdurahmon"
+
+console.log(abdurahmon);
 
 
+if(isOpen) {
+  isOpen = "a";
+}
+ */
 
+var i = 10;
 
+const ab = function () {
+  var i = 1;
+  console.log(i);
+}
 
-
+ab();
+console.log(i);
 
 
 
